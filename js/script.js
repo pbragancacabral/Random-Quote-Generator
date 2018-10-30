1fc29759de9a2773a58eae5c96a19a0913bdb369
Pedro de Bragança Cabral <pbragancacabral@gmail.com>
2018-10-30 17:49:40 +0100

    Add quotes, select a random one and display it

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
-// event listener to respond to "Show another quote" button clicks
-// when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
+const quotes = [
+    {
+        quote: "What comes, when it comes, will be what it is.",
+        source: "Alberto Caeiro"
+    },
+    {
+        quote: "I wasn’t meant for reality, but life came and found me.",
+        source: "Fernando Pessoa"
+    },
+    {
+        quote: "Words that come from the heart are never spoken, they get caught in the throat and can only be read in ones's eyes",
+        source: "José Saramago"
+    }
+];
+
+function getRandomQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomIndex];
+}
+
+function printQuote() {
+    let html = "";
+    const quote = getRandomQuote();
+    html += "<p class='quote'>" + quote.quote + "</p>";
+    html += "<p class='source'>" + quote.source + "</p>";
+    document.getElementById('quote-box').innerHTML = html;
+}
